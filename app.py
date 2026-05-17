2c10c283b736be1d0fd1b5bdc152b41a08fc69c7
Ukasha01 <ukasha.ghufran482@gmail.com>
2026-05-17 12:01:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ st.markdown("""
 }
 
 /* ══ MAIN APP BACKGROUND (LIGHT YELLOW) ══ */
-.stApp { background: #111827; }
+.stApp { background: #FDFCE9; }
 .block-container { padding: 2rem 2.4rem 3rem !important; max-width: 1480px !important; }
 
 /* ══ SIDEBAR (PREMIUM DARK) ══ */
